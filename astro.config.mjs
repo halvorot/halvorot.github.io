f803c7b0e7b0017f609935edc83f59dc9e1a4691
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-10-22 13:15:11 +0200

    Added tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://halvorot.github.io'
-})
+  site: 'https://halvorot.github.io',
+  integrations: [tailwind()]
+});
