2c7a8850e3ff05be9ae0d04a279b7c4644ea5fc7
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-10-24 21:24:29 +0200

    Refactor nav with data from js file and map func

---
+++ src/data/navData.js
@@ -0,0 +1,20 @@
+const navData = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "Capabilities",
+        path: "/capabilities"
+    },
+    {
+        name: "Experience",
+        path: "/experience"
+    },
+    {
+        name: "Contact",
+        path: "https://www.linkedin.com/in/halvor-teigen/"
+    },
+]
+
+export default navData;
