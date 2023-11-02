eb0a1b26251b98e08816f5e148ab31fdbd29a8c3
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-11-02 16:31:02 +0100

    added animation to cert banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,17 @@ const colors = require('tailwindcss/colors')
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce-left': 'bounce-left 1s ease-in-out 2',
+      },
+      keyframes: {
+        'bounce-left': {
+          '0%, 100%': { transform: 'translateX(0)'},
+          '50%': { transform: 'translateX(-25%)'}
+        }
+      },  
+    },
   },
   plugins: [],
 };
