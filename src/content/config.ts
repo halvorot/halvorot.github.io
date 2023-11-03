d05dedbd61e0b0d2ca976ad82b365936802c997c
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-11-03 18:24:08 +0100

    Added experience page

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,10 +3,21 @@ import { z, defineCollection } from 'astro:content';
 export const collections = {
   'certifications': defineCollection({
     type: 'data',
-    schema: ({image}) => z.object({
+    schema: ({ image }) => z.object({
       name: z.string(),
       image: image(),
       certificateLink: z.string(),
+    }),
+  }),
+  'experience': defineCollection({
+    type: 'data',
+    schema: z.object({
+      startDate: z.date(),
+      endDate: z.date().optional(),
+      heading: z.string(),
+      subHeading: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).optional(),
     }),
   }),
 };
