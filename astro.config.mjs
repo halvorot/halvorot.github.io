f01610ea0ca05ce3bf2423cb6a98d4bbfd287d60
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-11-11 01:56:22 +0100

    Add Google analytics and cookie consent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://halvorot.github.io',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(
+    {
+      filter: (page) => page !== 'https://halvorot.github.io/privacy-policy/',
+    }
+  ), solidJs()]
 });
